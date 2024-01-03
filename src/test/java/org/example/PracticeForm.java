f58fcd5103163841f2d6b9c4c07a0af01b69f6de
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-03 07:03:42 +0600

    Automation Testing

--- src/test/java/org/example/PracticeForm.java
+++ src/test/java/org/example/PracticeForm.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class PracticeForm extends DriverSetup{
@@ -17,6 +20,38 @@ public class PracticeForm extends DriverSetup{
         System.out.println(browser.switchTo());
         System.out.println(browser.manage());
         System.out.println(browser.navigate());
+
+    }
+
+    @Test
+    public void PracticeFormPage() throws InterruptedException {
+        browser.get("https://demoqa.com/automation-practice-form");
+        WebElement firstName = browser.findElement(By.id("firstName"));
+        firstName.sendKeys("Mohammad");
+        Thread.sleep(500);
+
+        WebElement lastName = browser.findElement(By.id("lastName"));
+        lastName.sendKeys("Sumon");
+        Thread.sleep(500);
+
+        WebElement email = browser.findElement(By.xpath("(//input[@id='userEmail'])[1]"));
+        email.sendKeys("mohammad.sumon9897@gmail.com");
+        Thread.sleep(500);
+
+        WebElement gender = browser.findElement(By.xpath("(//label[normalize-space()='Male'])[1]"));
+        gender.click();
+        Thread.sleep(200);
+
+        WebElement phone = browser.findElement(By.id("userNumber"));
+        phone.sendKeys("1631057540");
+
+        WebElement dobDay = browser.findElement(By.id("dateOfBirthInput"));
+        dobDay.clear();
+        dobDay.sendKeys("25 October 1999");
+        Thread.sleep(500);
+        
+
+
 
     }
 
